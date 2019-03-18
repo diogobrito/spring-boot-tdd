2610876bfdfe23fb48d9539fc3456f3251fdef02
devaws <devaws@wvdi-lnx011.brasilprev.corp>
2019-03-18 17:45:07 -0300

    Final aula 11

---
+++ src/main/java/com/example/demo/repository/helper/PessoaRepositoryImpl.java
@@ -0,0 +1,4 @@
+package com.example.demo.repository.helper;
+
+public class PessoaRepositoryImpl {
+}
---
+++ src/main/java/com/example/demo/repository/helper/PessoaRepositoryQueries.java
@@ -0,0 +1,4 @@
+package com.example.demo.repository.helper;
+
+public interface PessoaRepositoryQueries {
+}
